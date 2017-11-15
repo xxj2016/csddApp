c380584f0b006bbcaa824ca16a924178eff8ab60
Jet Xu <jet.xu@agileact.com>
2017-11-15 11:01:07 +0800

    ionic3分类菜单设计(类似外卖)

--- src/pages/contact/contact.module.ts
+++ src/pages/contact/contact.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from "./../../components/components.module";
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { ContactPage } from "./contact";
@@ -7,6 +8,7 @@ import { ContactPage } from "./contact";
         ContactPage
     ],
     imports: [
+        ComponentsModule,
         IonicPageModule.forChild(ContactPage)
     ]
 })
