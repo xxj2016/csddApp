accb547fb35c7510db381aeec060c6a6dc93e938
Jet Xu <jet.xu@agileact.com>
2017-11-15 18:19:47 +0800

    ionic3个人中心界面设计

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class TabsPage {
 
   tab1Root = 'HomePage';
-  tab2Root = 'AboutPage';
-  tab3Root = 'ContactPage';
+  tab2Root = 'ContactPage';
+  tab3Root = 'AboutPage';
 
   constructor() {
 
